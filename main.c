5192231fd7004df82dbd60c4e050127e8b5462c1
ufdjensen <daniel.jensen@ufl.edu>
2015-09-20 16:41:35 -0400

    MSP1: Clean code add comments for sonar

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 #include<stdint.h>
 /*
  * main.c
+ * Interfaces with the Sonar sensor and a timer counter just fine, but needs to be rewritten to use pin interrupts
+ * instead of input capture
  */
 uint16_t distcycles;
 float distin;
 uint16_t before;
-int sonarcount = 0;
+int sonarcount;
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
     P1DIR = 0x01;    //Set Port 1 Pin 1 as an output
@@ -32,29 +34,27 @@ __interrupt void Timer_A(void)
 {
 	if(sonarcount%2 == 0)
 	{
-		before = TACCR0;  //rising edge
+		before = TA0CCR0;  //rising edge
 	}
 	else
 	{
-		distcycles = TACCR0 - before;  //falling edge
-		distin = distcycles * 0.00676;
+		distcycles = TA0CCR0 - before;  //automatically calculates the difference due to unsigned arith. no sign problems
+		distin = distcycles * 0.00676;  //Divide by 148 to conver microseconds to inches with this part
 	}
 	sonarcount++;
+
+	/*
+	 * To get this working consider the following initializations:
+	 *
+	 * uint16_t distcycles;
+		float distin;
+		uint16_t before;
+	 *  P1DIR = 0x01;    //Set Port 1 Pin 1 as an output
+    	P1SEL |= 0x02; //set P1.1 as CCInput
+    	P1SEL2 = 0x00;  //Samesies
+    	TACTL = 0x220; //Set Timercounter A To run in continuous mode
+    	TACCTL0 = 0xC910;
+    	__enable_interrupt();// sets global ifg
+	 */
 }
 
-/*P1OUT = 0x01;
-
-		__delay_cycles(500);
-		P1OUT &= 0x00;
-		while(P1IN && 0x02 == 0)
-		{
-			__delay_cycles(1);  ///I should configure a pin interrupt in the int set a vaariable to the change and get out/disable interrupt
-		}
-		before = TA0R;
-		while(P1IN && 0x02 == 0x02)
-			{
-			__delay_cycles(1);  //The dummy program gets stuck here if the ultrasonic sensor doesn't respond the first time
-			}
-		distcycles = TA0R - before;
-		distin = distcycles*.000422; //fclock = 16mhz and microseconds/148 -> inches
-		__delay_cycles(1000000);*/
