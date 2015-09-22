76c87bb59951c48b7818702f28fc3e3b77a58e79
ufdjensen <daniel.jensen@ufl.edu>
2015-09-22 13:59:49 -0400

    SONAR: Replace CC functionality with pin interrupts

--- main.c
+++ main.c
@@ -4,20 +4,19 @@
 #include<stdint.h>
 /*
  * main.c
- * Interfaces with the Sonar sensor and a timer counter just fine, but needs to be rewritten to use pin interrupts
- * instead of input capture
+ * Interfaces with the Sonar sensor and a timer counter using pin interrupts
  */
 uint16_t distcycles;
 float distin;
 uint16_t before;
+uint16_t after;
 int sonarcount;
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-    P1DIR = 0x01;    //Set Port 1 Pin 1 as an output
-    P1SEL |= 0x02; //set P1.1 as CCInput
-    P1SEL2 = 0x00;  //Samesies
+    P1DIR = 0x01;    //Set Port 1 Pin 0 as an output
     TACTL = 0x220; //Set Timercounter A To run in continuous mode
-    TACCTL0 = 0xC910;
+    P1IE = 0x02; // Enable interrupt Port1Pin1
+    P1IES = 0x02; //P1.1 triggers an interrupt on a high to low transition
     __enable_interrupt();// sets global ifg
 
 	while(1)
@@ -25,36 +24,18 @@ int main(void) {
 		P1OUT = 0x01;
 		__delay_cycles(500);
 		P1OUT &= 0x00;
+		before = TA0R;
 		__delay_cycles(1000000);
 	}
 	return 0;
 }
-#pragma vector=TIMER0_A0_VECTOR
-__interrupt void Timer_A(void)
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void)
 {
-	if(sonarcount%2 == 0)
-	{
-		before = TA0CCR0;  //rising edge
-	}
-	else
-	{
-		distcycles = TA0CCR0 - before;  //automatically calculates the difference due to unsigned arith. no sign problems
-		distin = distcycles * 0.00676;  //Divide by 148 to conver microseconds to inches with this part
-	}
-	sonarcount++;
 
-	/*
-	 * To get this working consider the following initializations:
-	 *
-	 * uint16_t distcycles;
-		float distin;
-		uint16_t before;
-	 *  P1DIR = 0x01;    //Set Port 1 Pin 1 as an output
-    	P1SEL |= 0x02; //set P1.1 as CCInput
-    	P1SEL2 = 0x00;  //Samesies
-    	TACTL = 0x220; //Set Timercounter A To run in continuous mode
-    	TACCTL0 = 0xC910;
-    	__enable_interrupt();// sets global ifg
-	 */
+	after = TA0R;					// Set the TA0R to a variable to make subtraction work
+	distcycles = after-before;		//Find width of the pulse
+	distin = distcycles * 0.00675;  //  uS/148 = inches
+	P1IFG = 0x00;					//Clear the interrupt flag
 }
 
