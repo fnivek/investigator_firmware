181b0db2d53503104379da2e819d34d38f4a2249
Kevin French <fnivek@gmail.com>
2015-11-10 19:14:07 -0500

    ENCODER: Cleanup file

--- encoder.c
+++ encoder.c
@@ -8,22 +8,21 @@
 #include "encoder.h"
 
 // Globals
-int32_t encodercount1 = 0;//these longs keep track of motor position for left and right respectively
+int32_t encodercount1 = 0;		//these longs keep track of motor position for left and right respectively
 int32_t encodercount2 = 0;
-uint16_t compare1old = 0;//These are registers used to hold the CC register value for speed calculation
+uint16_t compare1old = 0;		//These are registers used to hold the CC register value for speed calculation
 uint16_t compare2old = 0;
-uint16_t diff1 = 0;//This is the number of clock ticks that have occured in 1/898 of a revolution
+uint16_t diff1 = 0;				//This is the number of clock ticks that have occured in 1/898 of a revolution
 uint16_t diff2 = 0;
 uint16_t timer1intcode;
-uint16_t a_diff2[120];
 
 //P2.1 is the left encoder signal
 //P2.4 is the right encoder signal
 void Init_Encoder ()
 {
 
-	P2DIR &= 0xED; //Set P2.1 and 2.4 as inputs CCI1A and CCI2A
-	P2SEL |= 0x12; //Each pin needs function code 01
+	P2DIR &= 0xED; 		//Set P2.1 and 2.4 as inputs CCI1A and CCI2A
+	P2SEL |= 0x12; 		//Each pin needs function code 01
 	P2SEL2 &= 0xED;
 	TA1CTL = 0x02E0;	//Make TA1 a 16bit counter /8
 	TA1CCTL1 = 0x4910;	//rising edge use ccIxA compare capture enable interrupts
@@ -34,19 +33,20 @@ void Init_Encoder ()
 #pragma vector=TIMER1_A1_VECTOR
 __interrupt void ENCODER2(void)
 {
-	timer1intcode=TA1IV;//Reading this register clears the interrupt flag
+	timer1intcode = TA1IV;				//Reading this register clears the interrupt flag
 	if((timer1intcode & 0x02) == 0x02)
 	{
 		//Interrupt for left encoder
 		__delay_cycles(1);
 		if(ldir_active == 1)
 		{
-			encodercount1++; //Dir = 1 means forward, this conditional keeps track of "position"
+			encodercount1++; 			//Dir = 1 means forward, this conditional keeps track of "position"
 		}
 		else
 		{
 			encodercount1--;
 		}
+
 		diff1 = TA1CCR1 - compare1old; //Difference is for speed calculation
 
 		compare1old = TA1CCR1;
@@ -58,21 +58,14 @@ __interrupt void ENCODER2(void)
 		__delay_cycles(1);
 		if(rdir_active == 1)
 		{
-			encodercount2++; //Dir = 1 means  //******* USED TO BE ++ *********
+			encodercount2++; 			//Dir = 1 means  //******* USED TO BE ++ *********
 		}
 		else
 		{
 			encodercount2--;
 		}
 
-		diff2 = TA1CCR2 - compare2old; //This difference will be used for speed computation
-
-		if(encodercount2 < 121 && encodercount2 > 0)
-		{
-
-			//ccr1[encodercount2] = timer1value;
-			a_diff2[encodercount2] = diff2;
-		}
+		diff2 = TA1CCR2 - compare2old; 	//This difference will be used for speed computation
 
 		compare2old = TA1CCR2;
 	}
