fc487e870a8c3b1300d33b187cbc9f71b79f094b
Kevin French <fnivek@gmail.com>
2015-11-19 22:38:14 -0500

    ENCODER: Add not moving check

--- encoder.c
+++ encoder.c
@@ -15,6 +15,8 @@ uint16_t compare2old = 0;
 uint16_t diff1 = 0;				//This is the number of clock ticks that have occured in 1/898 of a revolution
 uint16_t diff2 = 0;
 uint16_t timer1intcode;
+uint8_t ovf_count_1 = 0;
+uint8_t ovf_count_2 = 0;
 
 //P2.1 is the left encoder signal
 //P2.4 is the right encoder signal
@@ -24,7 +26,7 @@ void Init_Encoder ()
 	P2DIR &= 0xED; 		//Set P2.1 and 2.4 as inputs CCI1A and CCI2A
 	P2SEL |= 0x12; 		//Each pin needs function code 01
 	P2SEL2 &= 0xED;
-	TA1CTL = 0x02E0;	//Make TA1 a 16bit counter /8
+	TA1CTL = 0x02E0 | TAIE;	//Make TA1 a 16bit counter /8
 	TA1CCTL1 = 0x4910;	//rising edge use ccIxA compare capture enable interrupts
 	TA1CCTL2 = 0x4910;
 
@@ -34,7 +36,32 @@ void Init_Encoder ()
 __interrupt void ENCODER2(void)
 {
 	timer1intcode = TA1IV;				//Reading this register clears the interrupt flag
-	if((timer1intcode & 0x02) == 0x02)
+	// Overflow interrupt
+	if(timer1intcode == TA1IV_TAIFG)
+	{
+		++ovf_count_1;
+		++ovf_count_2;
+
+
+		// If we've overflowed twice assume the motors are stoped
+		if(ovf_count_1 >= 2)
+		{
+			diff1 = 0xFFFF;
+		}
+
+		if(ovf_count_2 >= 2)
+		{
+			diff2 = 0xFFFF;
+		}
+
+		//TA1CTL &= ~TAIFG;
+
+		return;
+
+	}
+
+
+	if(timer1intcode == TA1IV_TACCR1)
 	{
 		//Interrupt for left encoder
 		__delay_cycles(1);
@@ -50,9 +77,11 @@ __interrupt void ENCODER2(void)
 		diff1 = TA1CCR1 - compare1old; //Difference is for speed calculation
 
 		compare1old = TA1CCR1;
+		ovf_count_1 = 0;
+		return;
 	}
 
-	if((timer1intcode & 0x04) == 0x04)
+	if(timer1intcode == TA1IV_TACCR2)
 	{
 		//Interrupt for the right encoder
 		__delay_cycles(1);
@@ -68,6 +97,8 @@ __interrupt void ENCODER2(void)
 		diff2 = TA1CCR2 - compare2old; 	//This difference will be used for speed computation
 
 		compare2old = TA1CCR2;
+		ovf_count_2 = 0;
+		return;
 	}
 
 
